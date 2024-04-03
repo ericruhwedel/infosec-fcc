1e132992186ab3ae3a1d11173d1a1e0aaca6be3b
Eric Ruhwedel <eric.ruhwedel@gmail.com>
2024-04-03 16:23:35 -0500

    updating app

--- myApp.js
+++ myApp.js
@@ -4,8 +4,18 @@ const app = express();
 
 const PORT = process.env.PORT || 3030;
 
-app.listen(PORT, () => {
-  console.log('Eric Ruhwedel InfoSec | started on port ${PORT}');
+
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Eric Ruhwedel InfoSec | started on port ${port}');
 });
 
 
